b61047f9b83ed982ec969548360df03428e2ca50
aswanikumarv <aswanikumar_v@outlook.com>
2020-02-27 08:56:51 +0530

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Aswani Kumar V on 27/02/20.
+//  Copyright © 2020 Aswani Kumar V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
