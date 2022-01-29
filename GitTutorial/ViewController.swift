1d36a530fc70c4ae5770fe32c86e6c9ff22222b1
Aswanikumar <aswanikumar_v@outlook.com>
2022-01-29 07:55:02 +0530

    Added print message in viewdidload

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let message = "Hello Git!"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(message)
     }
 
 
